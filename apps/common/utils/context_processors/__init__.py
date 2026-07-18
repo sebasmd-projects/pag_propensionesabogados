9cb0eeaaecc7c99b461fe99dad384b593ca3ffb2
Sebastian Morales PC-escritorio <sebasmoralesd@gmail.com>
2026-07-18 14:43:19 -0500

    fix: order services imgs

--- apps/common/utils/context_processors/__init__.py
+++ apps/common/utils/context_processors/__init__.py
@@ -6,6 +6,36 @@ from django.utils.translation import gettext_lazy as _
 def custom_processors(request: Any) -> dict[str, Any]:
     PRACTICE_AREAS: list[dict[str, str]] = [
         {
+            "image": "pensiones_negadas.webp",
+            "subject": _("Denied Pensions"),
+            "alt": _("Denied Pensions"),
+        },
+        {
+            "image": "insolvencia_economica.webp",
+            "subject": _("Economic Insolvency"),
+            "alt": _("Economic Insolvency"),
+        },
+        {
+            "image": "derecho_laboral.webp",
+            "subject": _("Labor Law"),
+            "alt": _("Labor Law"),
+        },
+        {
+            "image": "derecho_civil.webp",
+            "subject": _("Civil Law"),
+            "alt": _("Civil Law"),
+        },
+        {
+            "image": "derecho_penal.webp",
+            "subject": _("Criminal Law"),
+            "alt": _("Criminal Law"),
+        },
+        {
+            "image": "derecho_internacional.webp",
+            "subject": _("International Law"),
+            "alt": _("International Law"),
+        },
+        {
             "image": "analisis_de_riesgos.webp",
             "subject": _("Risk Analysis"),
             "alt": _("Risk Analysis"),
@@ -31,45 +61,16 @@ def custom_processors(request: Any) -> dict[str, Any]:
             "alt": _("Legal Department"),
         },
         {
-            "image": "derecho_civil.webp",
-            "subject": _("Civil Law"),
-            "alt": _("Civil Law"),
-        },
-        {
             "image": "derecho_de_familia.webp",
             "subject": _("Family Law"),
             "alt": _("Family Law"),
-        },
-        {
-            "image": "derecho_internacional.webp",
-            "subject": _("International Law"),
-            "alt": _("International Law"),
-        },
-        {
-            "image": "derecho_laboral.webp",
-            "subject": _("Labor Law"),
-            "alt": _("Labor Law"),
-        },
-        {
-            "image": "derecho_penal.webp",
-            "subject": _("Criminal Law"),
-            "alt": _("Criminal Law"),
-        },
-        {
-            "image": "insolvencia_economica.webp",
-            "subject": _("Economic Insolvency"),
-            "alt": _("Economic Insolvency"),
         },
         {
             "image": "investigacion_de_campo.webp",
             "subject": _("Field Investigation"),
             "alt": _("Field Investigation"),
         },
-        {
-            "image": "pensiones_negadas.webp",
-            "subject": _("Denied Pensions"),
-            "alt": _("Denied Pensions"),
-        },
+
     ]
 
     ctx: dict[str, Any] = {}
