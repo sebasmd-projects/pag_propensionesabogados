2dafeeed08328ec600698ad4279e21bce97f516b
Sebastian Morales PC-escritorio <sebasmoralesd@gmail.com>
2026-08-28 19:31:00 -0500

    feat: resize service images

--- public/staticfiles/assets/imgs/convert_to_webp.py
+++ public/staticfiles/assets/imgs/convert_to_webp.py
@@ -6,7 +6,7 @@ from PIL import Image
 ROOT_DIR = Path(".")
 
 # Extensiones de imágenes soportadas (sin incluir webp porque ese es el destino)
-SUPPORTED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".mp4"]
+SUPPORTED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".mp4", ".jfif"]
 
 def convert_to_webp(root_dir):
     for path in root_dir.rglob("*"):
